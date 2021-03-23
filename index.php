2867aac36fffff8d10c89561e73f62fed6943bcc
niinimps <74964638+niinimps@users.noreply.github.com>
2021-03-23 10:02:29 +0200

    Update index.php
    title lisätty

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start();
 
 <html>
 	<head>
-		<title>Horizons by TEMPLATED</title>
+		<title>Stressiveks</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
